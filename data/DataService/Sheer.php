b8715f3ad6b3a695d3c7240724aa5706c571b40c
Andy Kirk <andy@andykirk.net>
2022-05-12 13:31:07 +0100

    Add image info.

--- data/DataService/Sheer.php
+++ data/DataService/Sheer.php
@@ -14,11 +14,11 @@ class Sheer extends DataServiceDB
 {
 	public function __construct()
 	{
-		parent::__construct();        
-        
+		parent::__construct();
+
         $hostname  = 'localhost';
         $jhostname = 'localhost';
-        
+
         if (DEV) {
             $database  = 'sheer_dev';
             $jdatabase = 'jan_dev';
@@ -26,7 +26,7 @@ class Sheer extends DataServiceDB
         } elseif (TEST) {
             $database  = 'sheer_test';
             $jdatabase = 'jan_test';
-            
+
         } else {
             $database  = 'sheer';
             $jdatabase = 'jan';
@@ -41,28 +41,44 @@ class Sheer extends DataServiceDB
 
         $this->database  = $database;
         $this->jdatabase = $jdatabase;
-        
+
         $this->dao = new PDO("mysql:host=$hostname;dbname=$database", $username, $password, array(
             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8;'
         ));
-        
+
         $this->jan_dao = new PDO("mysql:host=$jhostname;dbname=$jdatabase", $jusername, $jpassword, array(
             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8;'
         ));
-        
+
         $this->main_table  = '`' . $database . '`.`sheer_data`';
-        
-        
+
+
         $this->base_sql    = 'SELECT d.*, b.primary_colour, b.secondary_colour, b.tertiary_colour, b.logo_svg, b.logo_svg_with_fallback, b.logo_svg_path, b.logo_png_path, b.params';
 		$this->base_sql   .= "\n" . ' FROM ' . $this->main_table . ' d';
 		$this->base_sql   .= "\n" . ' JOIN `' . $jdatabase . '`.`jancore_brands` b ON d.alias = b.alias';
 	}
-	
-    
+
+    public function postQuery($data)
+    {
+        $public_root_path  = realpath($_SERVER['DOCUMENT_ROOT']) . DIRECTORY_SEPARATOR;
+        foreach ($data as &$item) {
+            $item['image_info'] = getimagesize($public_root_path . $item['logo_png_path']);
+
+            $w = $item['image_info'][0];
+            $h = $item['image_info'][1];
+            $image_ratio = ($w < $h) ? ($w / $h) : ($h / $w);
+
+            $item['image_info']['ratio'] = $image_ratio;
+        }
+
+        return $data;
+    }
+
+
 	public function getState($value)
 	{
 		return $this->parseValue($value, 'd.state', '%s');
 	}
-	
+
 
 }
